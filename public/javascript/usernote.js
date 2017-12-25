24e4e255a90720a49130d4406204558e341a601c
Gregory T Draheim <GregoryTDraheim@gmail.com>
2017-12-24 22:27:07 -0500

    logout button functioning

--- public/javascript/usernote.js
+++ public/javascript/usernote.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	$(document).on("click", ".log-out", logoutFunc);
+
 	getUser();
 
 	// Function for retrieving user info
@@ -10,24 +12,36 @@ $(document).ready(function() {
 
 			if (data.firstName && data.lastName) {
 
-				$("#signed-in").html("Signed in as " + data.firstName +" "+ data.lastName);
+				appendDropdown(data.firstName + " " + data.lastName);
 
 			} else if (data.firstName && !data.lastName){
 
-				$("#signed-in").html("Signed in as " + data.firstName);
+				appendDropdown(data.firstName);
 
 			} else if (!data.firstName && data.lastName){
 
-				$("#signed-in").html("Signed in as " + data.lastName);
+				appendDropdown(data.lastName);
 
 			} else {
 
-				$("#signed-in").html("Signed in as " + data.email);
+				appendDropdown(data.email)
+
 			}
 
-			
 			
 		});
 	}
 
+	function appendDropdown(name) {
+		$("#signed-in").html("");
+		$("#signed-in").append('<div class="nametag-drop"><button type="" class="btn nametag-drop dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' + name + '<span class="caret"></span></button><ul class="dropdown-menu"><li role="separator" class="divider"></li><li><a href="/store.html">My Account</a></li><li role="separator" class="divider"></li><li><a href="/menu.html">My Purchases</a></li><li role="separator" class="divider"></li><li class="log-out"><a href="/index.html">Logout</a></li></ul></div> ');
+	}
+
+	function logoutFunc() {
+		$.get('/logout', function() {
+			console.log("logged out");
+		})
+	}
+
 });
+
